95101697da8fe96f3028698a059ea2ced6d7135b
Kasem Arnontavilas <mrtomyum@gmail.com>
2017-03-21 20:28:06 +0700

    Update Readme.md

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ How to use?
 ===========
 Please Remark first section of code to test the traditional pattern that will complete job within 18-20 sec.
 Then swap to the first section and see the performance for your self.
+
+Notice: On the line 183 and 202 there are a sleep time to simulate i/o and service call delay as in real life transaction.
+If we remove this line performance will be very fast because it use map object to deal with transform(), this is a real power of go!
+As you can see in optimize go routine code it still have delay time simulated but the performance is not effect from i/o or service delay any more
--- main.go
+++ main.go
@@ -1,136 +1,14 @@
-package main
-
-import (
-	"os"
-	"encoding/csv"
-	"strconv"
-	"time"
-	"fmt"
-	"log"
-	"sync"
-)
-
-type Product struct {
-	PartNumber string
-	UnitCost   float64
-	UnitPrice  float64
-}
-
-type Order struct {
-	CustomerNumber int
-	PartNumber     string
-	Quantity       int
-
-	UnitCost  float64
-	UnitPrice float64
-}
-
-func main() {
-	start := time.Now()
-
-	extractCh := make(chan *Order)
-	transformCh := make(chan *Order)
-	doneCh := make(chan bool)
-
-	go extract(extractCh)
-	go transform(extractCh, transformCh)
-	go load(transformCh, doneCh)
-
-	<- doneCh
-	fmt.Println(time.Since(start))
-}
-
-func extract(ch chan *Order) {
-	f, _ := os.Open("./orders.txt")
-	defer f.Close()
-	r := csv.NewReader(f)
-
-	for field, err := r.Read(); err == nil; field, err = r.Read() {
-		order := new(Order)
-		order.CustomerNumber, _ = strconv.Atoi(field[0])
-		order.PartNumber = field[1]
-		order.Quantity, _ = strconv.Atoi(field[2])
-		ch <- order
-	}
-	close(ch)
-}
-
-func transform(extractCh, transformCh chan *Order) {
-	f, _ := os.Open("./productList.txt")
-	defer f.Close()
-	r := csv.NewReader(f)
-
-	records, _ := r.ReadAll()
-	productList := make(map[string]*Product)
-	for _, field := range records {
-		product := new(Product)
-		product.PartNumber = field[0]
-		product.UnitCost, _ = strconv.ParseFloat(field[1], 64)
-		product.UnitPrice, _ = strconv.ParseFloat(field[2], 64)
-		productList[product.PartNumber] = product
-	}
-	w := sync.WaitGroup{}
-	for o := range extractCh {
-		//numMessage++
-		w.Add(1)
-		go func(o *Order) {
-			time.Sleep(3 * time.Millisecond)
-			o.UnitCost = productList[o.PartNumber].UnitCost
-			o.UnitPrice = productList[o.PartNumber].UnitPrice
-			transformCh <- o
-			w.Done()
-		}(o)
-	}
-
-	w.Wait()
-	log.Println("finish Transform")
-	close(transformCh)
-}
-
-func load(tranformCh chan *Order, doneCh chan bool) {
-	f, _ := os.Create("./dest.txt")
-	defer f.Close()
-	//numMessages := 0
-	w := sync.WaitGroup{}
-	fmt.Fprintf(f, "%20s%15s%12s%12s%15s%15s",
-		"Part Number", "Quantity", "Unit Cost", "Unit Price", "Total Cost", "total Price")
-
-	for o := range tranformCh {
-		//numMessages++
-		w.Add(1)
-		go func(o *Order) {
-			time.Sleep(1 * time.Millisecond)
-			fmt.Fprintf(f, "%20s %15d %12.2f %12.2f %15.2f %15.2f\n",
-				o.PartNumber, o.Quantity, o.UnitCost, o.UnitPrice,
-				o.UnitCost*float64(o.Quantity),
-				o.UnitPrice*float64(o.Quantity))
-			//numMessages--
-			w.Done()
-		}(o)
-	}
-	w.Wait()
-	doneCh <- true
-}
-
-// 4.6.1 - Initial ETL with synchronous processing
-// Solution แรกที่ทำงานด้วยกระบวนการปกติแบบ Serialize โดยการอ่านไฟล์ แก้ไข และโหลดข้อมูลลงไฟล์ปลายทาง
 //package main
 //
 //import (
-//	"encoding/csv"
-//	"fmt"
 //	"os"
+//	"encoding/csv"
 //	"strconv"
 //	"time"
+//	"fmt"
+//	"log"
+//	"sync"
 //)
-//
-//func main() {
-//	start := time.Now()
-//	orders := extract()
-//	orders = transform(orders)
-//	load(orders)
-//	fmt.Println(time.Since(start))
-//}
 //
 //type Product struct {
 //	PartNumber string
@@ -147,64 +25,186 @@ func load(tranformCh chan *Order, doneCh chan bool) {
 //	UnitPrice float64
 //}
 //
-//func extract() []*Order {
-//	result := []*Order{}
+//func main() {
+//	start := time.Now()
 //
+//	extractCh := make(chan *Order)
+//	transformCh := make(chan *Order)
+//	doneCh := make(chan bool)
+//
+//	go extract(extractCh)
+//	go transform(extractCh, transformCh)
+//	go load(transformCh, doneCh)
+//
+//	<- doneCh
+//	fmt.Println(time.Since(start))
+//}
+//
+//func extract(ch chan *Order) {
 //	f, _ := os.Open("./orders.txt")
 //	defer f.Close()
 //	r := csv.NewReader(f)
 //
-//	for record, err := r.Read(); err == nil;record, err = r.Read() {
+//	for field, err := r.Read(); err == nil; field, err = r.Read() {
 //		order := new(Order)
-//		order.CustomerNumber, _ = strconv.Atoi(record[0])
-//		order.PartNumber = record[1]
-//		order.Quantity, _ = strconv.Atoi(record[2])
-//		result = append(result, order)
+//		order.CustomerNumber, _ = strconv.Atoi(field[0])
+//		order.PartNumber = field[1]
+//		order.Quantity, _ = strconv.Atoi(field[2])
+//		ch <- order
 //	}
-//	return result
+//	close(ch)
 //}
 //
-//func transform(orders []*Order) []*Order {
+//func transform(extractCh, transformCh chan *Order) {
 //	f, _ := os.Open("./productList.txt")
 //	defer f.Close()
 //	r := csv.NewReader(f)
 //
 //	records, _ := r.ReadAll()
 //	productList := make(map[string]*Product)
-//	for _, record := range records {
+//	for _, field := range records {
 //		product := new(Product)
-//		product.PartNumber = record[0]
-//		product.UnitCost, _ = strconv.ParseFloat(record[1], 64)
-//		product.UnitPrice, _ = strconv.ParseFloat(record[2], 64)
+//		product.PartNumber = field[0]
+//		product.UnitCost, _ = strconv.ParseFloat(field[1], 64)
+//		product.UnitPrice, _ = strconv.ParseFloat(field[2], 64)
 //		productList[product.PartNumber] = product
 //	}
-//
-//	for idx, _ := range orders {
-//		time.Sleep(3 * time.Millisecond)
-//		o := orders[idx]
-//		o.UnitCost = productList[o.PartNumber].UnitCost
-//		o.UnitPrice = productList[o.PartNumber].UnitPrice
+//	w := sync.WaitGroup{}
+//	for o := range extractCh {
+//		//numMessage++
+//		w.Add(1)
+//		go func(o *Order) {
+//			time.Sleep(3 * time.Millisecond)
+//			o.UnitCost = productList[o.PartNumber].UnitCost
+//			o.UnitPrice = productList[o.PartNumber].UnitPrice
+//			transformCh <- o
+//			w.Done()
+//		}(o)
 //	}
 //
-//	return orders
+//	w.Wait()
+//	log.Println("finish Transform")
+//	close(transformCh)
 //}
 //
-//func load(orders []*Order) {
+//func load(tranformCh chan *Order, doneCh chan bool) {
 //	f, _ := os.Create("./dest.txt")
 //	defer f.Close()
+//	//numMessages := 0
+//	w := sync.WaitGroup{}
+//	fmt.Fprintf(f, "%20s%15s%12s%12s%15s%15s",
+//		"Part Number", "Quantity", "Unit Cost", "Unit Price", "Total Cost", "total Price")
 //
-//	fmt.Fprintf(f, "%20s%15s%12s%12s%15s%15s\n",
-//		"Part Number", "Quantity",
-//		"Unit Cost", "Unit Price",
-//		"Total Cost", "Total Price")
-//
-//	for _, o := range orders {
-//		time.Sleep(1 * time.Millisecond)
-//		fmt.Fprintf(f, "%20s %15d %12.2f %12.2f %15.2f %15.2f\n",
-//			o.PartNumber, o.Quantity,
-//			o.UnitCost, o.UnitPrice,
-//			o.UnitCost*float64(o.Quantity),
-//			o.UnitPrice*float64(o.Quantity))
+//	for o := range tranformCh {
+//		//numMessages++
+//		w.Add(1)
+//		go func(o *Order) {
+//			time.Sleep(1 * time.Millisecond)
+//			fmt.Fprintf(f, "%20s %15d %12.2f %12.2f %15.2f %15.2f\n",
+//				o.PartNumber, o.Quantity, o.UnitCost, o.UnitPrice,
+//				o.UnitCost*float64(o.Quantity),
+//				o.UnitPrice*float64(o.Quantity))
+//			//numMessages--
+//			w.Done()
+//		}(o)
 //	}
+//	w.Wait()
+//	doneCh <- true
 //}
+
+ //4.6.1 - Initial ETL with synchronous processing
+ //Solution แรกที่ทำงานด้วยกระบวนการปกติแบบ Serialize โดยการอ่านไฟล์ แก้ไข และโหลดข้อมูลลงไฟล์ปลายทาง
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	orders := extract()
+	orders = transform(orders)
+	load(orders)
+	fmt.Println(time.Since(start))
+}
+
+type Product struct {
+	PartNumber string
+	UnitCost   float64
+	UnitPrice  float64
+}
+
+type Order struct {
+	CustomerNumber int
+	PartNumber     string
+	Quantity       int
+
+	UnitCost  float64
+	UnitPrice float64
+}
+
+func extract() []*Order {
+	result := []*Order{}
+
+	f, _ := os.Open("./orders.txt")
+	defer f.Close()
+	r := csv.NewReader(f)
+
+	for record, err := r.Read(); err == nil;record, err = r.Read() {
+		order := new(Order)
+		order.CustomerNumber, _ = strconv.Atoi(record[0])
+		order.PartNumber = record[1]
+		order.Quantity, _ = strconv.Atoi(record[2])
+		result = append(result, order)
+	}
+	return result
+}
+
+func transform(orders []*Order) []*Order {
+	f, _ := os.Open("./productList.txt")
+	defer f.Close()
+	r := csv.NewReader(f)
+
+	records, _ := r.ReadAll()
+	productList := make(map[string]*Product)
+	for _, record := range records {
+		product := new(Product)
+		product.PartNumber = record[0]
+		product.UnitCost, _ = strconv.ParseFloat(record[1], 64)
+		product.UnitPrice, _ = strconv.ParseFloat(record[2], 64)
+		productList[product.PartNumber] = product
+	}
+
+	for idx, _ := range orders {
+		time.Sleep(3 * time.Millisecond)
+		o := orders[idx]
+		o.UnitCost = productList[o.PartNumber].UnitCost
+		o.UnitPrice = productList[o.PartNumber].UnitPrice
+	}
+
+	return orders
+}
+
+func load(orders []*Order) {
+	f, _ := os.Create("./dest.txt")
+	defer f.Close()
+
+	fmt.Fprintf(f, "%20s%15s%12s%12s%15s%15s\n",
+		"Part Number", "Quantity",
+		"Unit Cost", "Unit Price",
+		"Total Cost", "Total Price")
+
+	for _, o := range orders {
+		time.Sleep(1 * time.Millisecond)
+		fmt.Fprintf(f, "%20s %15d %12.2f %12.2f %15.2f %15.2f\n",
+			o.PartNumber, o.Quantity,
+			o.UnitCost, o.UnitPrice,
+			o.UnitCost*float64(o.Quantity),
+			o.UnitPrice*float64(o.Quantity))
+	}
+}
 
